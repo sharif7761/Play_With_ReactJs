bbb7b663cff2f0d84f0c66297330c0e725a8956a
sharif7761 <sharif7761@gmail.com>
2021-05-18 01:58:48 +0600

    boilingVerdict component

--- src/components/BoilingVerdict.js
+++ src/components/BoilingVerdict.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const BoilingVerdict = ({celsius = 0}) => {
+const BoilingVerdict = ({ celsius = 0 }) => {
     if(celsius >= 100) {
-        return <p>The water would boil</p>
+        return <p>The water would boil in this temperature</p>
     }
-    return <p>The water would no boil</p>
+    return <p>The water would no boil in this temperature</p>
 };
 
 export default BoilingVerdict;
